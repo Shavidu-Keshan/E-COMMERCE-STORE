225781b96f152ffcf17ae2a8c339f47380519eeb
Shavidu Keshan <shavidu4321@gmail.com>
2024-09-12 20:24:10 +0530

    crete route handler for sign up, loggin, logout

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,16 @@
 import express from "express";
-import dotenv from "dotenv"//import .env
+import dotenv from "dotenv"; // import .env
+import authRoutes from "./routes/auth.route.js"; 
 
-dotenv.config(); //cal the config function
+dotenv.config(); // Call the config function
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.use ("/api/auth", authRoutes)
+app.use("/api/auth", authRoutes);
 
-
-app.listen(5000, () => {
-  console.log("Server is running on http://localhost:" + PORT);
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
+
