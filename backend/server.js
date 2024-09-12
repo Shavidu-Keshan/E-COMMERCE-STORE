4216478dff4635c6004799a0795fd9d8d8918baa
Shavidu Keshan <shavidu4321@gmail.com>
2024-09-12 17:44:53 +0530

    create auth.route file

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,15 @@
 import express from "express";
+import dotenv from "dotenv"//import .env
+
+dotenv.config(); //cal the config function
 
 const app = express();
-const PORT =5000;
+
+const PORT = process.env.PORT || 5000;
+
+app.use ("/api/auth", authRoutes)
+
 
 app.listen(5000, () => {
-  console.log("Server is running on port 5000");
+  console.log("Server is running on http://localhost:" + PORT);
 });
