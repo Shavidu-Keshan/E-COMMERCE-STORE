92e376061eca285bc8b6693b1bbbcfa3bfc3a66e
Shavidu Keshan <shavidu4321@gmail.com>
2024-09-13 12:45:07 +0530

    create auth controllers

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,7 +1,22 @@
-export const signup = async (req, res) => {
-  res.send("Sign up route called");
-};
+import User from "../models/user.model";
 
+
+export const signup = async (req, res) => {
+  const {email,password,name} = req.body
+  try {
+    const userExists = await User.findOne({email});
+
+  if(userExists) {
+    return res.status(400).json({message:"User already exists"});
+  }
+  const user = await User.create({name,email,password});
+
+  res.status(200).json({message:"User created successfully",user});
+}
+   catch (error) {
+    res.status(500).json({message:"Something went wrong",error});
+  }
+}
 export const login = async (req, res) => {
   res.send("login route called");
 };
@@ -9,3 +24,4 @@ export const login = async (req, res) => {
 export const logout = async (req, res) => {
   res.send("logout route called");
 };
+
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv"; // import .env
 import authRoutes from "./routes/auth.route.js"; 
+import { connectDB } from "./lib/db.js";
 
 dotenv.config(); // Call the config function
 
@@ -8,9 +9,12 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json());//allow you to parse body of the requestt
+
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
+  connectDB();
 });
 
