b4acefcf13f27f47f9e74c315efa4ac0c7efaa8f
Shavidu Keshan <shavidu4321@gmail.com>
2024-09-15 19:34:50 +0530

    create product controlled ,auth middelware ,product model,product routr for products

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv"; // import .env
 import authRoutes from "./routes/auth.route.js"; 
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
+import productRoutes from "./routes/product.route.js";
 
 dotenv.config(); // Call the config function
 
@@ -14,6 +15,7 @@ app.use(express.json());//allow you to parse body of the requestt
 app.use(cookieParser());//allow you to parse cookie of the requestt
 
 app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
