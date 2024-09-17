49bb6398719d9a78c90bc7a82719aa621550b324
Shavidu Keshan <shavidu4321@gmail.com>
2024-09-17 20:21:35 +0530

    create coupon model.js

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import productRoutes from "./routes/product.route.js";
 import cartRoutes from "./routes/cart.route.js";
+import couponRoutes from "./routes/coupon.route.js";
 dotenv.config(); // Call the config function
 
 const app = express();
@@ -17,6 +18,7 @@ app.use(cookieParser());//allow you to parse cookie of the requestt
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart",cartRoutes);
+app.use("/api/coupons",couponRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
