d77554fcdf4b5380b74b348f4d1ff4a314bc29e0
Shavidu Keshan <shavidu4321@gmail.com>
2024-09-18 13:49:14 +0530

    complete payment par backend

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,8 @@ import cookieParser from "cookie-parser";
 import productRoutes from "./routes/product.route.js";
 import cartRoutes from "./routes/cart.route.js";
 import couponRoutes from "./routes/coupon.route.js";
+import paymentRoutes from "./routes/payment.route.js";
+
 dotenv.config(); // Call the config function
 
 const app = express();
@@ -19,6 +21,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart",cartRoutes);
 app.use("/api/coupons",couponRoutes);
+app.use("/api/payments", paymentRoutes)
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
