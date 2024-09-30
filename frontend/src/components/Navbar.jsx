fa3de6193a063395017f14fa6297f8a956055b9b
Shavidu Keshan <shavidu4321@gmail.com>
2024-09-30 18:37:00 +0530

    create form for sign up page

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,8 +4,8 @@ import { Link } from "react-router-dom";
 //import { useCartStore } from "../stores/useCartStore";
 
 const Navbar = () => {
-	const user = true;
-    const isAdmin = true;
+	const user = false;
+    const isAdmin = false;
 
 	return (
 		<header className='fixed top-0 left-0 w-full bg-gray-900 bg-opacity-90 backdrop-blur-md shadow-lg z-40 transition-all duration-300 border-b border-emerald-800'>
