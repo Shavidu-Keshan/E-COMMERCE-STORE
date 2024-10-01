95f4a96c133795a935eae264a571788b6101a237
Shavidu Keshan <shavidu4321@gmail.com>
2024-10-01 12:07:53 +0530

    Added logout functionality and user authentication check
    - Implemented `logout` function to clear user session on server-side and update state.
    - Added `checkAuth` function to verify user authentication status by fetching user profile.
    - Handled errors with toast notifications and ensured proper state management for authentication.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,24 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage.jsx";
 import SignUpPage from "./pages/SignUpPage.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
 import Navbar from "./components/Navbar.jsx";
 import { Toaster } from "react-hot-toast";
 import { useUserStore } from "./stores/useUserStore";
+import { useEffect } from "react";
+import LoadingSpinner from "./components/LoadingSpinner.jsx";
 function App() {
-  const {user} = useUserStore()
+  const {user,checkAuth, checkingAuth} = useUserStore();
+
+  useEffect(() => {
+    checkAuth();
+  },[checkAuth]);
+
+  if (checkingAuth) {
+    return <LoadingSpinner />;
+  }
+
+  if (checkingAuth) return <LoadingSpinner />;
 
   return (
     <div className='min-h-screen bg-gray-900 text-white relative overflow-hidden'>
@@ -22,8 +34,8 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={! user ? <SignUpPage /> : <Navigate to="/" />} />
+        <Route path="/login" element={! user ? <LoginPage /> : <Navigate to="/" />} />
       </Routes>
       </div>
       <Toaster />
--- frontend/src/stores/useUserStore.js
+++ frontend/src/stores/useUserStore.js
@@ -34,6 +34,26 @@ export const useUserStore = create((set,get) => ({
 			set({ loading: false });
 			toast.error(error.response.data.message || "An error occurred");
 		}
+	},
+
+	logout: async () => {
+		try {
+			await axios.post("/auth/logout");
+			set({ user: null });
+		} catch (error) {
+			toast.error(error.response?.data?.message || "An error occurred during logout");
+		}
+	},
+
+	checkAuth: async () => {
+		set({ checkingAuth: true });
+		try {
+			const response = await axios.get("/auth/profile");
+			set({ user: response.data, checkingAuth: false });
+		} catch (error) {
+			console.log(error.message);
+			set({ checkingAuth: false, user: null });
+		}
 	}
 
 }));
