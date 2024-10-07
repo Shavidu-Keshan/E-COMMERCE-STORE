d6a461020af17e3e9d95335c65b6c617bb1c2f7a
Shavidu Keshan <shavidu4321@gmail.com>
2024-10-07 09:40:47 +0530

    building cart page function

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -1,21 +1,23 @@
-import React from 'react';
-import toast from 'react-hot-toast';
-import { ShoppingCart } from 'lucide-react';
-import { useUserStore } from '../stores/useUserStore';
-const ProductCard = ({ product}) => {
-    const {user} = useUserStore();
-    const handleAddToCart = () => {
-        if (!user) {
-            toast.error("please login to add to cart",{id :"login"});
-            return;
-        }else{
+import toast from "react-hot-toast";
+import { ShoppingCart } from "lucide-react";
+import { useUserStore } from "../stores/useUserStore";
+import { useCartStore } from "../stores/useCartStore";
 
-        }
-        
-    }
-    
-  return (
-    <div className='flex w-full relative flex-col overflow-hidden rounded-lg border border-gray-700 shadow-lg'>
+const ProductCard = ({ product }) => {
+	const { user } = useUserStore();
+	const { addToCart } = useCartStore();
+	const handleAddToCart = () => {
+		if (!user) {
+			toast.error("Please login to add products to cart", { id: "login" });
+			return;
+		} else {
+			// add to cart
+			addToCart(product);
+		}
+	};
+
+	return (
+		<div className='flex w-full relative flex-col overflow-hidden rounded-lg border border-gray-700 shadow-lg'>
 			<div className='relative mx-3 mt-3 flex h-60 overflow-hidden rounded-xl'>
 				<img className='object-cover w-full' src={product.image} alt='product image' />
 				<div className='absolute inset-0 bg-black bg-opacity-20' />
@@ -38,7 +40,6 @@ const ProductCard = ({ product}) => {
 				</button>
 			</div>
 		</div>
-  )
-}
-
+	);
+};
 export default ProductCard;
